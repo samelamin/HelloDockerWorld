08b5d8af654bbf7fb2a08d4a268379c46b689cd3
Sam Elamin <hussam.elamin@gmail.com>
2015-11-25 21:39:22 +0000

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.Logging;
 
-namespace KWebStartup
+namespace HelloWeb
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+            app.UseIISPlatformHandler();
             app.UseStaticFiles();
             app.UseWelcomePage();
         }
     }
-}
